05f31b7edd324decda5b00f34f470140de791207
KielGanteng <hendy300408@gmail.com>
2025-08-20 11:56:08 +0700

    feat: update types

--- src/Types.tsx
+++ src/Types.tsx
@@ -1,11 +1,21 @@
 // types.ts - Shared type definitions
 
-export interface User {
-    id: string;
-    firstName: string;
-    lastName: string;
-    address: string;
-    identityNumber: number;
-    birthDate: Date;
-    status: boolean;
-  }
+// export interface User {
+//     id: string;
+//     firstName: string;
+//     lastName: string;
+//     address: string;
+//     identityNumber: number;
+//     birthDate: Date;
+//     status: boolean;
+//   }
+
+
+export interface Events {
+  id: number;
+  title: string;
+  description: string;
+  date: string;
+  created_at: Date;
+  updated_at: Date;
+}
